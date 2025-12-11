5482f0b88463f48b31d7dbb91119a023e2655dc9
Juan Carlos <jucarbelmez@gmail.com>
2025-12-11 08:28:44 +0100

    actualizo la pagina de peliculas

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+pelisFav = [];
+miStorage = window.localStorage;
+entrado = false;
+
 function maquetarPelis(peliculas){
-        let imagen;
         for(let peli of peliculas){
             contenedor = document.getElementById("contenedor");
             miDiv = document.createElement("div");       
@@ -8,8 +11,7 @@ function maquetarPelis(peliculas){
             );
             texto = document.createElement("h2");
             img = document.createElement("img");
-            imagen.createElement("img");
-            imagen.onerror = (e) => e.target.src = "/img/error imagen.png";
+            img.onerror = (e) => e.target.src = "/img/errorimagen.png";
 
             img.src = peli.Poster; 
             texto.textContent = peli.Title;
@@ -17,7 +19,6 @@ function maquetarPelis(peliculas){
             miDiv.appendChild(img);
             miDiv.appendChild(texto);
             contenedor.appendChild(miDiv);
-
     }
 }
 
@@ -30,8 +31,10 @@ function LanzaPeticionDetalle(id){
         released = document.createElement("h3");
         plot = document.createElement("p");
         boton = document.createElement("button");
+        img2 = document.createElement("img"); 
 
-
+      
+        img2.src = "./img/corazon.png";
         boton.innerHTML = "Cerrar";
         plot.innerHTML = data.Plot;
         released.innerHTML = "Salida: " + data.Released;
@@ -39,23 +42,60 @@ function LanzaPeticionDetalle(id){
         img.src = data.Poster;
         miDiv2.id = "detalle";
 
-
+        img2.addEventListener("click",() => {
+            if(pelisFav.includes(data.imdbID)){
+                img2.src = "./img/corazon.png";
+                pelisFav.splice(pelisFav.indexOf(data.imdbID),1);
+                miStorage.setItem("PelisFavoritas", JSON.stringify(pelisFav));
+            }
+            else{
+                img2.src = "./img/corazonrojo.png";
+                pelisFav.push(data.imdbID);
+                miStorage.setItem("PelisFavoritas", JSON.stringify(pelisFav));
+                entrado = true;
+            }   
+        });
+        
+       
+        if(entrado){ 
+            if(pelisFav.includes(data.imdbID))
+                img2.src = "./img/corazonrojo.png";
+            else{
+                img2.src = "./img/corazon.png";
+            }
+        }
+        
        
         miDiv2.appendChild(img);
         miDiv2.appendChild(texto);
         miDiv2.appendChild(released);
         miDiv2.appendChild(plot);
         miDiv2.appendChild(boton);
+        miDiv2.appendChild(img2);
 
         boton.addEventListener("click", () => {
             miDiv2.innerHTML = " ";
             miDiv2.style.visibility = "hidden";
         })
 
+
         contenedor.appendChild(miDiv2);
+
+         
 
     })
 }
+
+function maquetarFavoritos(id){
+    for(peli of id){
+    fetch("https://www.omdbapi.com/?i=" + peli + "&apikey=5ddaee62").then(response => response.json()).then(data => {
+            maquetarPelis([data]); 
+            peticionEnCurso = false;
+        })
+    }
+}
+
+
 
 function LanzaPeticion(url){
     if(!peticionEnCurso){
@@ -68,16 +108,24 @@ function LanzaPeticion(url){
     }
 }
 
-
 window.onload = () => {
+    if(!miStorage.getItem("PelisFavoritas")){
+        miStorage.setItem("PelisFavoritas", JSON.stringify([]));
+        pelisFav = [];
+    }
+    else
+        pelisFav = JSON.parse(miStorage.getItem("PelisFavoritas"));
+
     peticionEnCurso = false;
-    boton = document.getElementById("botonCargar");
     pelicula = document.getElementById("pelicula");
     buscar = document.getElementById("botonbuscar");
     landing = document.getElementById("botonLanding");
     buscador = document.getElementById("buscador");
-    inicio = document .getElementById("landing");
+    inicio = document.getElementById("landing");
+    año = document.getElementById("Año");
+    tipo = document.getElementById("tipo");
     var contador = 2;
+    favoritos = document.getElementById("favoritos");
 
     landing.addEventListener("click",() => {
         buscador.style.visibility = "visible";
@@ -88,8 +136,21 @@ window.onload = () => {
     buscar.addEventListener("click", ()  => {
         contenedor.innerHTML = " ";
         contador = 2;
-         LanzaPeticion("https://www.omdbapi.com/?s=" + pelicula.value +"&apikey=5ddaee62&page=1");
-            boton.style.visibility = "visible";
+
+        if(tipo.value=="cualquiera"){
+            tipoBusqueda = "";
+        }else if(tipo.value == "movie"){
+            tipoBusqueda = "&type=movie";
+        }else if(tipo.value == "series"){
+            tipoBusqueda = "&type=series";
+        }
+
+        if(!año.value){
+         LanzaPeticion("https://www.omdbapi.com/?s=" + pelicula.value + tipoBusqueda + "&apikey=5ddaee62&page=1");
+        }
+        else{
+         LanzaPeticion("https://www.omdbapi.com/?s=" + pelicula.value + tipoBusqueda + "&y=" + año.value +"&apikey=5ddaee62&page=1");   
+        }
            
     })
 
@@ -98,11 +159,30 @@ window.onload = () => {
         if(pelicula.value.length >= 3){
         contenedor.innerHTML = " ";
         contador = 2;
-        LanzaPeticion("https://www.omdbapi.com/?s=" + pelicula.value +"&apikey=5ddaee62&page=1");
+
+        if(tipo.value=="cualquiera"){
+            tipoBusqueda = "";
+        }else if(tipo.value == "movie"){
+            tipoBusqueda = "&type=movie";
+        }else if(tipo.value == "series"){
+            tipoBusqueda = "&type=series";
+        }
+
+        if(!año.value){
+         LanzaPeticion("https://www.omdbapi.com/?s=" + pelicula.value + tipoBusqueda + "&apikey=5ddaee62&page=1");
+        }
+        else{
+         LanzaPeticion("https://www.omdbapi.com/?s=" + pelicula.value + tipoBusqueda + "&y=" + año.value +"&apikey=5ddaee62&page=1");   
+        }
+
         }
    })
 
 
+   favoritos.addEventListener("click", () => {
+        contenedor.innerHTML = " ";
+        maquetarFavoritos(pelisFav);
+   })
 }
 
 var contador = 2;
@@ -115,4 +195,3 @@ window.onscroll = () =>
       contador++;
    }
 }
-
